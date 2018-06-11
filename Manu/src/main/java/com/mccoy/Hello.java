43d32bacba262e6bf099fcbd9e759419dbb75a45
ashokmccoy <ashokkumarkn400@gmail.com>
2018-06-11 18:19:38 +0530

    update commit

---
+++ Manu/src/main/java/com/mccoy/Hello.java
@@ -0,0 +1,10 @@
+package com.mccoy;
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("main is started.....");
+		System.out.println("main is ended.....");
+	}
+	
+
+}
