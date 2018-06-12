9edfbbb9eb5397313aaa5e70ccee6dd0995df5f7
ashokmccoy <ashokkumarkn400@gmail.com>
2018-06-12 09:51:42 +0530

    demo class

---
+++ Manu/src/main/java/com/mccoy/demo.java
@@ -0,0 +1,12 @@
+package com.mccoy;
+
+public class demo {
+	public static void main(String[] args) {
+		System.out.println("main is started.....");
+		System.out.println("maccoy");
+		
+		System.out.println("main is ended.....");
+	}
+	
+
+}
