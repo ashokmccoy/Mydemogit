9bf9e34eeb62afb1f0d7985fa1dd195c3cd2c92c
ashokmccoy <ashokkumarkn400@gmail.com>
2018-06-12 10:53:56 +0530

    new

---
+++ Ashok_Maven/src/main/java/com/mccoy/demo2.java
@@ -0,0 +1,12 @@
+package com.mccoy;
+
+public class demo2 {
+	public static void main(String[] args) {
+		System.out.println("main is started.....");
+		System.out.println("maccoy");
+		System.out.println("maccoy");
+		System.out.println("main is ended.....");
+	}
+	
+
+}
