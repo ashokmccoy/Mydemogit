f784087aeeb5af865cb2ac7676034caaa2f0902e
ashokmccoy <ashokkumarkn400@gmail.com>
2018-06-12 10:17:15 +0530

    changes

--- Manu/src/main/java/com/mccoy/demo.java
+++ Manu/src/main/java/com/mccoy/demo.java
@@ -4,7 +4,7 @@ public class demo {
 	public static void main(String[] args) {
 		System.out.println("main is started.....");
 		System.out.println("maccoy");
-		
+		System.out.println("maccoy");
 		System.out.println("main is ended.....");
 	}
 	
